1f6a8cf2e06da6f4702eccd053f374a5493167b7
Emmanuel Cocher <ecocher@fieldbox.ai>
2024-12-24 11:58:29 +0100

    [FEAT] Solve part2 of day15

--- src/day15.py
+++ src/day15.py
@@ -72,10 +72,7 @@ move_to_diff = {
 
 
 def one_move(
-    robot_pos: complex,
-    boxes: list[complex],
-    walls: set[complex],
-    move: str,
+    robot_pos: complex, boxes: list[complex], walls: set[complex], move: str
 ) -> tuple[complex, list[complex]]:
     ok_move = False
     boxes_index_to_move: set[int] = set()
@@ -90,6 +87,59 @@ def one_move(
         else:
             ok_move = True
             break
+    #
+    if ok_move:
+        robot_pos += diff
+        for i_box in boxes_index_to_move:
+            boxes[i_box] += diff
+    return robot_pos, boxes
+
+
+def one_move_part2(
+    robot_pos: complex, boxes: list[complex], walls: set[complex], move: str
+) -> tuple[complex, list[complex]]:
+    ok_move = False
+    boxes_index_to_move: set[int] = set()
+    #
+    diff = move_to_diff[move]
+    if move in ["<", ">"]:
+        # This is nearly the same thing as part 1
+        new_wall_pos = robot_pos + diff
+        new_box_pos = (robot_pos + diff) if (move == ">") else (robot_pos + 2 * diff)
+        while new_wall_pos not in walls:
+            if new_box_pos in boxes:
+                i_box = boxes.index(new_box_pos)
+                boxes_index_to_move.add(i_box)
+                new_wall_pos += 2 * diff
+                new_box_pos += 2 * diff
+            else:
+                ok_move = True
+                break
+    else:
+        coords_to_check: set[complex] = {robot_pos + diff}
+        diff1 = complex(0, 1) if move == "v" else complex(0, -1)
+        diff2 = complex(1, 1) if move == "v" else complex(1, -1)
+        while len(coords_to_check.intersection(walls)) == 0:
+            new_coords_to_check: set[complex] = set()
+            for coord in coords_to_check:
+                coord_other = coord + complex(-1, 0)
+                if coord in boxes:
+                    i_box = boxes.index(coord)
+                    boxes_index_to_move.add(i_box)
+                    new_coords_to_check.add(coord + diff1)
+                    new_coords_to_check.add(coord + diff2)
+                elif coord_other in boxes:
+                    i_box = boxes.index(coord_other)
+                    boxes_index_to_move.add(i_box)
+                    new_coords_to_check.add(coord_other + diff1)
+                    new_coords_to_check.add(coord_other + diff2)
+                else:
+                    pass
+            if len(new_coords_to_check) == 0:
+                ok_move = True
+                break
+            coords_to_check = new_coords_to_check
+
     #
     if ok_move:
         robot_pos += diff
@@ -121,11 +171,12 @@ def transform_board(
     return new_robot_pos, new_walls, new_boxes
 
 
-def solve_part_1(
+def solve(
     robot_initial_pos: complex,
     boxes_initial: list[complex],
     walls: set[complex],
     moves: str,
+    part1: bool = True,
 ) -> int:
     robot_pos: complex = robot_initial_pos
     boxes: list[complex] = list(boxes_initial)
@@ -135,8 +186,11 @@ def solve_part_1(
     for i_move, move in enumerate(moves):
         # print("~~~~~~~~~~~~~~~~")
         # print(f">>> move {i_move+1}/{n_moves}: {move!r}")
-        robot_pos, boxes = one_move(robot_pos, boxes, walls, move)
-    # print_board(robot_pos, walls, boxes)
+        if part1:
+            robot_pos, boxes = one_move(robot_pos, boxes, walls, move)
+        else:
+            robot_pos, boxes = one_move_part2(robot_pos, boxes, walls, move)
+    print_board(robot_pos, walls, boxes, part1=part1)
     #
     # Compte the sum of the GPS coordinates
     total: int = 0
@@ -148,23 +202,14 @@ def solve_part_1(
     return total
 
 
-def solve_part_2(
-    robot_initial_pos: complex,
-    boxes_initial: list[complex],
-    walls: set[complex],
-    moves: str,
-) -> int:
-    return 0
-
-
 if __name__ == "__main__":
-    with open("./inputs/day15/input2.txt") as f:
+    with open("./inputs/day15/input.txt") as f:
         lines = f.readlines()
     robot_pos, walls, boxes, moves = parse_inputs(lines)
     print_board(robot_pos, walls, boxes, part1=True)
-    res_part_1 = solve_part_1(robot_pos, boxes, walls, moves)
+    res_part_1 = solve(robot_pos, boxes, walls, moves, part1=True)
     print(f"{res_part_1=}")
     new_robot_pos, new_walls, new_boxes = transform_board(robot_pos, walls, boxes)
     print_board(new_robot_pos, new_walls, new_boxes, part1=False)
-    res_part_2 = solve_part_2(new_robot_pos, new_boxes, new_walls, moves)
+    res_part_2 = solve(new_robot_pos, new_boxes, new_walls, moves, part1=False)
     print(f"{res_part_2=}")
